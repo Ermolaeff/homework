d53e1737a94319e94a721c1419e472c53f774c01
Ermolaeff <ermolaevGleb@outlook.com>
2017-09-07 08:19:08 +0300

    Shorter version

--- main.c
+++ main.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
-
 int main(void)
 {
     float count;
@@ -11,37 +7,23 @@ int main(void)
 
     switch (name) {
     case 'C':
-        printf("%.2f F\n", count * 1.8 + 32);
-        printf("%.2f K\n", count + 273.15);
+        printf("%.2f F\n%.2f K\n", count * 1.8 + 32, count + 273.15);
         getch();
-
         break;
     case 'F':
-        printf("%.2f C\n", (count - 32) / 1.8);
-        printf("%.2f K\n", (count + 459.67)*5/9);
+        printf("%.2f C\n%.2f K\n", (count - 32) / 1.8, (count + 459.67)*5/9);
         getch();
-
         break;
     case 'K':
-        printf("%.2f C\n", count + 273.15);
-        printf("%.2f F\n", count * 9/5-459.67);
+        printf("%.2f C\n%.2f F\n", count + 273.15, count * 9/5-459.67);
         getch();
 
         break;
     default:
-        printf("%.2f C:\n", count);
-        printf("%.2f F\n", count * 1.8 + 32);
-        printf("%.2f K\n\n", count + 273.15);
-
-        printf("%.2f F:\n", count);
-        printf("%.2f C\n", (count - 32) / 1.8);
-        printf("%.2f K\n\n", (count + 459.67)*5/9);
-
-        printf("%.2f K:\n", count);
-        printf("%.2f C\n", count + 273.15);
-        printf("%.2f F\n\n", count * 9/5-459.67);
+        printf("%.2f C:\n%.2f F\n%.2f K\n\n%.2f F:\n%.2f C\n%.2f K\n\n%.2f K:\n%.2f C\n%.2f F\n\n",
+                count, count * 1.8 + 32, count + 273.15, count, (count - 32) / 1.8, (count + 459.67)*5/9,
+                count, count + 273.15, count * 9/5-459.67);
         getch();
-
         break;
     }
 
